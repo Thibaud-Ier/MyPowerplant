c056e2c475bff0da72a3f5d8581533628042fca9
Thibaud ROBERT <thibaud.robert@lunalogic.com>
2024-08-19 16:19:15 +0200

    Load value object

---
+++ Domain/ValueObjects/Load.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+
+namespace Domain.ValueObjects
+{
+    public class Load : ValueObject<Load>
+    {
+        public int Value { get; }
+
+        public Load(int value)
+        {
+            if (value < 0)
+                throw new InvalidOperationException();
+
+            Value = value;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
