37d843372e0c455d6c35e49a4cceb1dd1b4c447f
Thibaud ROBERT <thibaud.robert@lunalogic.com>
2024-08-19 17:31:54 +0200

    Add Wind ValueObject

---
+++ Domain/ValueObjects/Fuels/Wind.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.ValueObjects.Fuels
+{
+    public class Wind(Percent value) : Fuel
+    {
+        public override string Name => "Wind";
+
+        public override double Value { get; } = value.Rate;
+
+        public Percent Percent { get; } = value;
+    }
+}
---
+++ DomainTests/ValueObjects/Fuels/WindTests.cs
@@ -0,0 +1,22 @@
+﻿using Domain.ValueObjects;
+using Domain.ValueObjects.Fuels;
+
+namespace DomainTests.ValueObjects.Fuels
+{
+    public class WindTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(50)]
+        [InlineData(100)]
+        public void GivenAPercentWhenInitWindThenValueShouldBeEqualsThePercentRate(int value)
+        {
+            var percent = new Percent(value);
+
+            var wind = new Wind(percent);
+
+            Assert.Equal(wind.Value, percent.Rate);
+            Assert.Equal(wind.Percent, percent);
+        }
+    }
+}
