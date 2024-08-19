a344bf456b622b7493da308b66887b744aa36760
Thibaud ROBERT <thibaud.robert@lunalogic.com>
2024-08-19 17:40:00 +0200

    CO2 added

---
+++ Domain/ValueObjects/Fuels/CO2.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.ValueObjects.Fuels
+{
+    public class CO2(PositiveValue value) : Fuel
+    {
+        public override string Name => "CO2";
+
+        public override double Value { get; } = value.Value;
+    }
+}
